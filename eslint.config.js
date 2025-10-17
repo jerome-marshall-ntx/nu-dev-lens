371b8c35437f56ea25ae62815a7468d71bc1f1f6
Jerome Marshall <jerome.marshall@nutanix.com>
2025-10-17 15:59:05 +0530

    chore: enhance ESLint configuration with FlatCompat, update PostCSS and Prettier exports

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,22 @@
 import tseslint from "typescript-eslint";
-// @ts-ignore -- no types for this plugin
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+// @ts-expect-error -- no types for this plugin
 import drizzle from "eslint-plugin-drizzle";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default tseslint.config(
   {
     ignores: [".next", "next-env.d.ts"],
   },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     files: ["**/*.ts", "**/*.tsx"],
     plugins: {
