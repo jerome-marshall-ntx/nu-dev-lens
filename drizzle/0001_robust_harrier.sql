e46d87eb319f2e1d8459116be7564e427cc5e461
Jerome Marshall <jerome.marshall@nutanix.com>
2026-01-23 16:28:00 +0530

    refactor: remove unused database tables and columns to streamline schema and improve maintainability

--- drizzle/0001_robust_harrier.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "repository" DROP COLUMN "avatarUrl";--> statement-breakpoint
-ALTER TABLE "repository" DROP COLUMN "summary";--> statement-breakpoint
-ALTER TABLE "repository" DROP COLUMN "embedding";--> statement-breakpoint
-ALTER TABLE "repository" DROP COLUMN "rawData";
